51bd4c0ba6be874ea32e6d475a2bbbd42b5d9e7a
Victor Cano <vikkno123@gmail.com>
2022-02-07 20:27:11 -0600

    Updates

---
+++ precache-manifest.dab1cb62aed8dfc32c3efea6c6395c99.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "530b2818454ba4204b87",
+    "url": "/victorcano/static/css/main.60d73e7c.chunk.css"
+  },
+  {
+    "revision": "530b2818454ba4204b87",
+    "url": "/victorcano/static/js/main.8a3a4351.chunk.js"
+  },
+  {
+    "revision": "67bba0d921d9a90f9580",
+    "url": "/victorcano/static/js/runtime~main.51ecec9b.js"
+  },
+  {
+    "revision": "c859811630d59a998335",
+    "url": "/victorcano/static/js/2.21bc8458.chunk.js"
+  },
+  {
+    "revision": "8030a1e438ac52fb49b3363d03daf597",
+    "url": "/victorcano/static/media/smartLogIn.8030a1e4.jpg"
+  },
+  {
+    "revision": "54e3043fe2c7c31760d901d4e2d43944",
+    "url": "/victorcano/static/media/rubik.54e3043f.png"
+  },
+  {
+    "revision": "c94bf31d736955077c5d83d6847d491a",
+    "url": "/victorcano/static/media/CV_VictorCano.c94bf31d.pdf"
+  },
+  {
+    "revision": "2fcec13c262fc33d4fb5b9aec5685089",
+    "url": "/victorcano/index.html"
+  }
+];
